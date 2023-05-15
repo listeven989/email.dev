8543bc54a419db1f5a893f162064c02e1b809868
steven4354 <steven4354@gmail.com>
2023-05-14 17:44:09 -0700

    wip

--- packages/email-service/database/setup.sql
+++ packages/email-service/database/setup.sql
@@ -124,3 +124,12 @@ BEGIN
     DROP COLUMN subject;
   END IF;
 END $$;
+
+-- Create the users table
+CREATE TABLE IF NOT EXISTS users (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW()
+);

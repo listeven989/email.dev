7678dfe5116a83ac45ac587aaf2b357085b3ae9e
steven4354 <steven4354@gmail.com>
2023-05-26 21:28:35 -0700

    mailto check

--- packages/tracking-service/index.ts
+++ packages/tracking-service/index.ts
@@ -73,7 +73,7 @@ app.get("/link/:linkId", async (req, res) => {
     let url = result.rows[0].url;
 
     // check if url has https:// otherwise append it
-    if (!url.startsWith("https://") && !url.startsWith("http://")) {
+    if (!url.startsWith("https://") && !url.startsWith("http://") && !url.startsWith("mailto:")) {
       url = `https://${url}`;
     }
 
